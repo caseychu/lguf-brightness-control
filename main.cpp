9fe4ee43ec9fa5919e843c0cc7653e168111a890
Casey Chu <caseychu@stanford.edu>
2020-01-16 19:18:11 -0800

    Handle unplug/replugs

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ static const int HID_REPORT_TYPE_INPUT = 0x01;
 static const int HID_REPORT_TYPE_OUTPUT = 0x02;
 static const int HID_REPORT_TYPE_FEATURE = 0x03;
 
-float get_brightness(libusb_device_handle* handle)
-{
+float get_brightness(libusb_device_handle* handle) {
 	u_char data[8] = { 0x00 };
 	int bytes = libusb_control_transfer(handle,
 		LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE,
@@ -28,8 +27,7 @@ float get_brightness(libusb_device_handle* handle)
 	return float(brightness_value) / 54000.f;
 }
 
-void set_brightness(libusb_device_handle* handle, float brightness)
-{
+void set_brightness(libusb_device_handle* handle, float brightness) {
 	uint16_t brightness_value = max(min(int(brightness * 54000), 54000), 0);
 
 	u_char data[6] = {
@@ -43,17 +41,7 @@ void set_brightness(libusb_device_handle* handle, float brightness)
 		throw std::runtime_error("Unable to set brightness: libusb_control_transfer error.");
 }
 
-#include <iostream>
-#include <sstream>
-
-#define DBOUT( s )            \
-{                             \
-   std::wostringstream os_;    \
-   os_ << s;                   \
-   OutputDebugStringW( os_.str().c_str() );  \
-}
-
-static libusb_device_handle* get_lg_ultrafine() {
+libusb_device_handle* get_lg_ultrafine() {
 	const uint16_t vendor_id = 0x43e;
 	const uint16_t product_id = 0x9A63; // Use 0x9a40 for 5k
 
@@ -69,8 +57,6 @@ static libusb_device_handle* get_lg_ultrafine() {
 			libusb_free_device_list(devices, 1);
 			throw std::runtime_error("Failed to get device descriptor");
 		}
-
-		DBOUT(descriptor.idVendor << " " << descriptor.idProduct << "\r\n")
 
 		if (descriptor.idVendor == vendor_id && descriptor.idProduct == product_id) {
 			libusb_device_handle* handle;
@@ -89,57 +75,48 @@ static libusb_device_handle* get_lg_ultrafine() {
 }
 
 libusb_device_handle* init_lg_ultrafine() {
+	libusb_device_handle* handle = get_lg_ultrafine();
+	if (handle) {
+		libusb_set_auto_detach_kernel_driver(handle, 1);
+		int err = libusb_claim_interface(handle, 1);
+		if (err)
+			throw std::runtime_error("Failed to claim interface 1.");
+	}
+	return handle;
+}
+
+libusb_device_handle* handle = NULL;
+float brightness;
+
+void reinit_handle() {
+	if (handle != NULL)
+		libusb_close(handle);
+
+	try {
+		handle = init_lg_ultrafine();
+		if (handle != NULL)
+			brightness = get_brightness(handle);
+	} catch (std::runtime_error err) {
+		handle = NULL;
+	}
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
 	int err = libusb_init(NULL);
 	if (err)
 		throw std::runtime_error("Unable to initialize libusb.");
 
-	libusb_device_handle* handle = get_lg_ultrafine();
-	if (handle == NULL)
-		throw std::runtime_error("Failed to find LG Ultrafine");
-	libusb_set_auto_detach_kernel_driver(handle, 1);
-	err = libusb_claim_interface(handle, 1);
-	if (err)
-		throw std::runtime_error("Failed to claim interface 1.");
-
-	return handle;
-}
-
-
-libusb_device_handle* handle;
-float brightness;
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
-{
-	try {
-		handle = init_lg_ultrafine();
-		brightness = get_brightness(handle);
-	}
-	catch (std::runtime_error err) {
-		OutputDebugStringA(err.what());
-		OutputDebugStringA("\r\n");
-
-		if (handle) {
-			libusb_release_interface(handle, 1);
-			libusb_attach_kernel_driver(handle, 1);
-			libusb_close(handle);
-		}
-		libusb_exit(NULL);
-		return -1;
-	}
+	reinit_handle();
 
 	// Register the window class.
-	const wchar_t CLASS_NAME[] = L"Brightness Controller";
-
+	const wchar_t CLASS_NAME[] = L"LG Ultrafine Brightness";
 	WNDCLASS wc = { };
-
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
-
 	RegisterClass(&wc);
 
 	// Create the window.
-
 	HWND hwnd = CreateWindowEx(
 		0,                              // Optional window styles.
 		CLASS_NAME,                     // Window class
@@ -154,25 +131,23 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 		hInstance,  // Instance handle
 		NULL        // Additional application data
 	);
-
 	if (hwnd == NULL)
 		return 0;
 
 	//ShowWindow(hwnd, nCmdShow);
 
 	// Run the message loop.
-	MSG msg = { };
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
+	libusb_exit(NULL);
 	return 0;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg)
 	{
 	case WM_DESTROY:
@@ -180,28 +155,30 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		return 0;
 
 	case 1024:
-		brightness -= 0.01f;
-		set_brightness(handle, brightness);
+		if (handle) {
+			try {
+				brightness -= 0.01f;
+				set_brightness(handle, brightness);
+			} catch (std::runtime_error err) {
+				reinit_handle();
+			}
+		}
 		return 0;
 
 	case 1025:
-		brightness += 0.01f;
-		set_brightness(handle, brightness);
+		if (handle) {
+			try {
+				brightness += 0.01f;
+				set_brightness(handle, brightness);
+			} catch (std::runtime_error err) {
+				reinit_handle();
+			}
+		}
 		return 0;
 
-		/*
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-		EndPaint(hwnd, &ps);
-	}
-	return 0;*/
+	case WM_DEVICECHANGE:
+		reinit_handle();
 	}
 
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
-
